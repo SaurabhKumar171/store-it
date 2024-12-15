b836495f271dea2e18c512cfa95b3d1879994694
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-15 21:35:52 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB",
+    }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "png.pngtree.com",
+        pathname: "/**", // Allow all paths from this domain
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
